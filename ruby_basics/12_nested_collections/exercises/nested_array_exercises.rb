3d11828e46b3a9313c7253d9eb0d011f841e276b
Hey Cobra <114818470+HeyCobra@users.noreply.github.com>
2022-12-06 20:57:59 -0500

    adding solutions

--- ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
@@ -11,16 +11,33 @@ def blank_seating_chart(number_of_rows, seats_per_row)
 
   # NOTE: if one of the nested arrays is changed, the others should **not**
   # change with it
+
+
+  array2 = Array.new(number_of_rows) {Array.new(seats_per_row)}
+  return array2 
+
 end
+
+
+
 
 def add_seat_to_row(chart, row_index, seat_to_add)
   # take a chart (2d array)  and add seat_to_add to the end of the row that is
   # at row_index index of the chart, then return the chart
+
+  chart[row_index].push(seat_to_add)
+  return chart
+
 end
 
 def add_another_row(chart, row_to_add)
   # take a chart and add row_to_add to the end of the chart,
   # then return the chart.
+
+  chart.push(row_to_add)
+  return chart
+
+
 end
 
 def delete_seat_from_row(chart, row_index, seat_index)
@@ -28,18 +45,38 @@ def delete_seat_from_row(chart, row_index, seat_index)
   # the chart, then return the chart
 
   # Hint: explore the ruby docs to find a method for deleting from an array!
+
+  chart[row_index].delete_at(seat_index)
+  return chart
+
+
+
 end
 
 def delete_row_from_chart(chart, row_index)
   # take a chart and delete the row at row_index of the chart,
   # then return the chart
+
+  chart.delete_at(row_index)
+  return chart
+
 end
 
 def count_empty_seats(chart)
   # take a chart and return the number of empty (nil) seats in it
 
   # NOTE: `chart` should **not** be mutated
+
+  x = chart.length - 1
+  y = 0
+  
+  for i in (0..x)
+    y += chart[i].count(nil)
+  end
+return y
 end
+
+
 
 def find_favorite(array_of_hash_objects)
   # take an array_of_hash_objects and return the hash which has the key/value
@@ -55,4 +92,14 @@ def find_favorite(array_of_hash_objects)
 
   # TIP: there will only be a maximum of one hash in the array that will
   # return true to the :is_my_favorite? key
+  
+  x = array_of_hash_objects.length - 1
+
+  for i in (0..x)
+    if array_of_hash_objects[i].values == true
+      return array_of_hash_objects[i]
+    end
+  end
+
+
 end
